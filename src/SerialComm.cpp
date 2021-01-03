1f4139b1b414dddf59bbe2621505ea91501fe287
supernach <68354685+supernach@users.noreply.github.com>
2021-01-04 00:18:46 +0100

    Update SerialComm.cpp

--- src/SerialComm.cpp
+++ src/SerialComm.cpp
@@ -2,7 +2,7 @@
 
 SerialComm::SerialComm() : io(), serial(io)
 {
-
+    this->Init();
 }
 
 SerialComm::~SerialComm()
@@ -13,21 +13,36 @@ SerialComm::~SerialComm()
 
 bool SerialComm::Conectar(const char* puerto, uint16_t baudios)
 {
+    uint8_t esperaConexionOK = 0;
     try
     {
-        this->serial.open(puerto);
+        this->puerto = puerto;
+        this->baudios = baudios;
+        this->serial.open(this->puerto);
 
         if(this->serial.is_open())
         {
-            this->serial.set_option(boostAsio::serial_port_base::baud_rate(baudios));
-            this->serial.set_option(boostAsio::serial_port_base::parity(boostAsio::serial_port::parity::none));
-            this->serial.set_option(boostAsio::serial_port_base::character_size(boostAsio::serial_port::character_size(8)));
-            this->serial.set_option(boostAsio::serial_port_base::stop_bits(boostAsio::serial_port::stop_bits::one));
-            this->serial.set_option(boostAsio::serial_port_base::flow_control(boostAsio::serial_port::flow_control::none));
+            this->ConfigurarConexion();
 
             std::cout << "Serialcomm::Conectar() nPuerto= " << puerto << std::endl;
 
+            while(!esperaConexionOK)
+            {
+                this->Escribir("INIT");
+
+                while(!esperaConexionOK)
+                {
+                    if( (this->Leer().compare("ACK")) )
+                    {
+                        this->Escribir("RACK");
+                        esperaConexionOK = 1;
+                    }
+                }
+            }
+
             this->estadoComm = eEstadoComm::CONECTADO;
+
+            esperaConexionOK = 0;
 
             return true;
         }
@@ -66,9 +81,9 @@ void SerialComm::Desconectar()
 
 }
 
-std::size_t SerialComm::Escribir(std::string datos)
+uint16_t SerialComm::Escribir(std::string datos)
 {
-    std::size_t bytesEscritos = 0;
+    uint16_t bytesEscritos = 0;
     try
     {
         bytesEscritos = boostAsio::write(this->serial, boostAsio::buffer(datos.c_str(), datos.size()));
@@ -81,38 +96,12 @@ std::size_t SerialComm::Escribir(std::string datos)
         std::cout << "::Escribir Fallo: " << ex.what() << std::endl;
     }
 
-    if(bytesEscritos < 0)
-    {
-        bytesEscritos = 0;
-    }
     return bytesEscritos;
 }
 
-std::size_t SerialComm::EscribirCaracter(char caracter)
+std::string SerialComm::Leer(char delim)
 {
-    std::size_t bytesEscritos = 0;
-    char c = caracter;
-    try
-    {
-        bytesEscritos = boostAsio::write(this->serial, boostAsio::buffer(&c, 1));
-        this->estadoComm = eEstadoComm::BLOQUEADO;
-    }
-    catch(boost::system::system_error& ex)
-    {
-        this->estadoComm = eEstadoComm::ERR;
-        std::cout << "::Escribir Fallo: " << ex.what() << std::endl;
-    }
-
-    if(bytesEscritos < 0)
-    {
-        bytesEscritos = 0;
-    }
-    return bytesEscritos;
-}
-
-std::string SerialComm::LeerLinea()
-{
-    size_t len;
+    uint16_t len;
     boostAsio::streambuf b;
     std::string line;
 
@@ -120,7 +109,7 @@ std::string SerialComm::LeerLinea()
     {
         try
         {
-            len = boostAsio::read_until(this->serial, b, '\n');
+            len = boostAsio::read_until(this->serial, b, delim);
         }
         catch(boost::system::system_error& ex)
         {
@@ -178,7 +167,7 @@ char SerialComm::LeerCaracter()
 
     this->estadoComm = eEstadoComm::BLOQUEADO;
 
-    size_t len = boostAsio::read(this->serial, boostAsio::buffer(&c, 1));
+    uint16_t len = boostAsio::read(this->serial, boostAsio::buffer(&c, 1));
 
     this->estadoComm = eEstadoComm::STANDBY;
 
@@ -196,29 +185,33 @@ bool SerialComm::EstaLibre()
     else return false;
 }
 
-bool SerialComm::EstaConectado()
+
+
+//////////////// PRIVATE
+
+void SerialComm::Init()
 {
-    if(this->serial.is_open())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    this->estadoComm = eEstadoComm::DESCONECTADO;
 }
 
-
-
-
-
-void SerialComm::EscanearPuertos(uint16_t baudios)
+void SerialComm::EscanearPuertos()
 {
     uint16_t nPuertos = sizeof(this->nombrePuertos);
 
     for(uint8_t i = 0; i < nPuertos; i++)
     {
-            this->Conectar(this->nombrePuertos[i], baudios);
+            this->Conectar(this->nombrePuertos[i], this->baudios);
             this->Desconectar();
     }
+}
+
+
+void SerialComm::ConfigurarConexion()
+{
+    this->serial.set_option(boostAsio::serial_port_base::baud_rate(this->baudios));
+    this->serial.set_option(boostAsio::serial_port_base::parity(this->paridad));
+    this->serial.set_option(boostAsio::serial_port_base::character_size(boostAsio::serial_port::character_size(this->tamCaracter)));
+    this->serial.set_option(boostAsio::serial_port_base::stop_bits(this->bitsParada));
+    this->serial.set_option(boostAsio::serial_port_base::flow_control(this->controlFlujo));
+
 }
