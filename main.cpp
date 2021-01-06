cb4c44199325821e730f4ebb654ae220c23c5721
supernach <68354685+supernach@users.noreply.github.com>
2021-01-06 19:08:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
 #include <Aida64SharedMemory.h>
-#include <SerialComm.h>
+#include <SerialHard.h>
 
 int main()
 {
-    SerialComm commEsp8266;
+    SerialHard commEsp8266;
 
     //Aida64SharedMemory Aida64sm;
 
     //aida64.Mostrar();
-
-    while(!commEsp8266.Conectar("/COM3", 9600))
-    {
-
-    }
 
 
     while(1)
@@ -21,15 +16,7 @@ int main()
         //UNA VEZ CONECTADOS MANDO DATOS AL ESP, ME QUEDO BLOQUEADO ESPERANDO LA RESPUESTA OK DEL ESP
         //Y ENTONCES DESBLOQUEO
 
-        if(commEsp8266.EstaLibre())
-        {
-            std::cout << "commEsp8266::Escribir() nBytes= " << commEsp8266.Escribir("ESTA ES UNA FRASE con ESPACIOS") << std::endl;
-        }
-        else
-        {
-            std::cout << "commEsp8266::LeerLinea() Data= " << commEsp8266.Leer() << std::endl;
-        }
-
+        std::cout << commEsp8266.Leer(4) << std::endl;
       /*  for(uint32_t i = 0; i <= 100000; i++)
         {
 
