c5eeefeeee1d136a48e1c3150fa4a181d5815c5e
supernach <68354685+supernach@users.noreply.github.com>
2021-01-06 19:08:38 +0100

    Create SerialSoft.h

---
+++ include/SerialSoft.h
@@ -0,0 +1,16 @@
+#ifndef SERIALSOFT_H
+#define SERIALSOFT_H
+
+
+class SerialSoft
+{
+    public:
+        SerialSoft();
+        virtual ~SerialSoft();
+
+    protected:
+
+    private:
+};
+
+#endif // SERIALSOFT_H
